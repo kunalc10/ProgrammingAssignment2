079da4e38494d758947f55645f7573385cbbd0d7
kunalc10 <kunalc1021@gmail.com>
2020-10-01 02:25:49 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function mimics the similar functionality of caching
 
-## Write a short comment describing this function
+## Basically one function creates a special list which contains some coverage
+## These coverages are indexed with <<- operator so that the values of parent environment
+## can be changed
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    parent.inverse <- NULL
+    set <- function(y) {
+        x <<- y
+        parent.inverse <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(child.inverse) parent.inverse <<- child.inverse
+    getinverse <- function() parent.inverse
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
+    
 }
 
 
-## Write a short comment describing this function
-
+## Checks if inverse is present or not. If inverse is present, then does not compute it
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    inverse <- x$getinverse()
+    if(!is.null(inverse)) {
+        message("getting cached data")
+        return(inverse)
+    }
+    data <- x$get()
+    inverse <- solve(data, ...)
+    x$setinverse(inverse)
+    inverse
+    
 }
